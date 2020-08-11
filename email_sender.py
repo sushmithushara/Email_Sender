b92060d146c2c319c0dffa4664016e7aad7fe1a1
sushmithushara <sushmithushara@gmail.com>
2020-08-11 15:37:39 +0530

    Sends Email to/from

---
+++ email_sender.py
@@ -0,0 +1,19 @@
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html =Template(Path('index.html').read_text())
+
+email = EmailMessage()
+email['from'] = 'Sushmi Thushara'
+email['to'] = 'sushmithushara@gmail.com'
+email['subject'] = 'Excercise for ZTM - Python Class'
+email.set_content(html.substitute({'name' : 'Sushmi Thushara'}),'html')
+
+with smtplib.SMTP(host = 'smtp.yahoo.com', port = 587) as smtp:
+	smtp.ehlo()
+	smtp.starttls()
+	smtp.login('sushmithushara@yahoo.com','sushsuman@93')
+	smtp.send_message(email)
+	print("All good now")
